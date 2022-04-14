79282a1643a5391e70a9c647b51dd974a5e01488
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-04-14 20:49:28 +0530

    Error fixed

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -1,5 +1,24 @@
 import pickle
 import numpy as np
-import streamlint
+import streamlit
 
 loaded_model = pickle.load(open('training_model.sav', 'rb'))
+
+
+def prediction_data(input_data):
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+# standardize the input data
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
